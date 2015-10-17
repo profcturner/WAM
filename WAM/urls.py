a3344edcd7bd4222821b5cda542332f8cca05643
Colin Turner <c.turner@ulster.ac.uk>
2015-10-17 14:31:30 +0100

    Starting to build simple user views

--- WAM/urls.py
+++ WAM/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from loads import views
+
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- loads/views.py
+++ loads/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from .models import Staff
+from .models import Task
+from .models import Activity
+
+def index(request):
+    staff_list = Staff.objects.all()
+    load_info = []
+    for staff in staff_list:
+        load_info.append(staff.hours_by_semester())
+        
+    template = loader.get_template('loads/index.html')
+    context = RequestContext(request, {
+        'staff_list': staff_list,
+        'load_info' : load_info,
+    })
+    return HttpResponse(template.render(context))
+    
+
+    
