fe743c0597feb774bd6b95bc8aab7ab80d1971cb
Colin Turner <c.turner@ulster.ac.uk>
2015-11-02 19:36:20 +0000

    Add --urgent-only that only emails if incomplete tasks are less than a week away
    This functionality is to allow, for instance, for the default call to
    be made weekly which shows all incomplete tasks, and the —urgent-only
    call to be made daily which will suppress email of non urgent tasks.

--- loads/management/commands/email_reminders.py
+++ loads/management/commands/email_reminders.py
@@ -15,51 +15,77 @@ from loads.models import Staff
 from loads.models import Task
 from loads.models import TaskCompletion
 
+# And code to handle timezones
+from django.utils.timezone import utc
+import datetime
+
 class Command(BaseCommand):
     help = 'Emails staff if tasks are outstanding'
 
-    #def add_arguments(self, parser):
-    #    parser.add_argument('poll_id', nargs='+', type=int)
+    def add_arguments(self, parser):
+        parser.add_argument('--urgent-only',
+            action='store_true',
+            dest='urgent-only',
+            default=False,
+            help='Only email if some tasks are urgent (less than 7 days to deadline)')
 
+            
     def handle(self, *args, **options):
         #TODO needs some decent exception handling
         all_staff = Staff.objects.all()
         
+        count = 0
+        urgent_only = options['urgent-only']
         for staff in all_staff:
-            self.email_tasks_by_staff(staff)
+            if self.email_tasks_by_staff(staff, urgent_only=urgent_only):
+                count += 1
 
-        self.stdout.write('Reminders sent')
+        self.stdout.write(str(count)+ ' reminder(s) sent')
 
 
-    def email_tasks_by_staff(self, staff):
+    def email_tasks_by_staff(self, staff, urgent_only='false'):
         """docstring for email_tasks_by_staff"""
         all_tasks = staff.get_all_tasks()
 
         # We will create separate lists for those tasks that are complete
         combined_list_complete = []
         combined_list_incomplete = []
-    
+        
+        urgent_tasks = False
+        now = datetime.datetime.utcnow().replace(tzinfo=utc)
         for task in all_tasks:
             # Is it complete? Look for a completion model
             completion = TaskCompletion.objects.all().filter(staff=staff).filter(task=task)
             if len(completion) == 0:
+                # It isn't complete...
                 combined_item = [task, False]
                 combined_list_incomplete.append(combined_item)
+                # How long do we have left
+                seconds_left = (task.deadline - now).total_seconds()
+                # If a task is less than a week from deadline consider it urgent
+                if(seconds_left < 60*60*24*7):
+                    urgent_tasks = True
             else:
                 combined_item = [task, completion[0].when]
                 combined_list_complete.append(combined_item)
                 
+        # Don't nag staff with no tasks
         if len(combined_list_incomplete) == 0:
-            # Don't nag staff with no tasks
             return False
         
+        # Or if urgency is set to low and no tasks are urgent
+        if not urgent_tasks and urgent_only:
+            return False 
+        
         plaintext = get_template('loads/emails/task_reminders.txt')
-        html     = get_template('loads/emails/task_reminders.html')
+        html = get_template('loads/emails/task_reminders.html')
 
         context = Context({
             'staff': staff,
             'combined_list_incomplete': combined_list_incomplete,
-            'combined_list_complete': combined_list_complete, 
+            'combined_list_complete': combined_list_complete,
+            'urgent_tasks': urgent_tasks,
+            'urgent_only' : urgent_only, 
         })
 
         # TODO Remove hardcode from address
@@ -72,6 +98,7 @@ class Command(BaseCommand):
         email = EmailMultiAlternatives(subject, text_content, from_email, [to])
         email.attach_alternative(html_content, "text/html")
         email.send()
+        return True
 
 
     
