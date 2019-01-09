be83733f7253493a08ecd44664b44244499df4fe
Colin Turner <c.turner@ulster.ac.uk>
2019-01-09 15:29:28 +0000

    Allow an option for moderators and coordinators to be shown

--- loads/forms.py
+++ loads/forms.py
@@ -67,6 +67,7 @@ class ModulesIndexForm(forms.Form):
     )
     programme = forms.ModelChoiceField(queryset=Programme.objects.all(), required=False)
     lead_programme = forms.BooleanField(required=False, initial=False)
+    show_people = forms.BooleanField(required=False, initial=False)
 
 
 
