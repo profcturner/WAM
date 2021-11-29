94c2e207d540b3023a2cd818fcaecf59e19673f5
Professor Colin Turner <c.turner@ulster.ac.uk>
2021-11-29 21:11:55 +0000

    When a superuser is detected without a staff object, make them one
    The command line utility creates a User object but with no corresponding superuser, this creates a problem for fresh installations. Creating a Staff object allows a login to occur,

--- loads/decorators.py
+++ loads/decorators.py
@@ -9,9 +9,17 @@ def staff_only(function):
         try:
             staff = Staff.objects.get(user=request.user)
         except Staff.DoesNotExist:
-            # We should redirect really, but for now
-            raise PermissionDenied("""Your user has no matching staff object.
-            This is likely to be because no account has been created for you.""")
+            # Is this a Superuser
+            if request.user.is_superuser:
+                # This is likely a created superuser with no matching Staff object. Create one.
+                staff = Staff.objects.create(user=request.user, staff_number=request.user.get_username(), title="",
+                                             package=None)
+                # Ok, let them through now.
+                return function(request, *args, **kwargs)
+            else:
+                # We should redirect really, but for now
+                raise PermissionDenied("""Your user has no matching staff object.
+                This is likely to be because no account has been created for you.""")
         else:
             return function(request, *args, **kwargs)
 
